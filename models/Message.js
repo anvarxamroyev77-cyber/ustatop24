4ec3d4284c26ea57666f4f5271168854ba418668
anvarxamroyev77-cyber <anvarxamroyev77@gmail.com>
2025-09-07 22:07:18 +0500

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,1 +1,8 @@
-
+const mongoose = require("mongoose");
+const MessageSchema = new mongoose.Schema({
+  text: String,
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  name: String,
+  createdAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model("Message", MessageSchema);
