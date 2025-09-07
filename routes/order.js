378d66c2efd6747b53f92609b3acfbba52c17131
anvarxamroyev77-cyber <anvarxamroyev77@gmail.com>
2025-09-07 22:11:46 +0500

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -1,1 +1,11 @@
-
+const mongoose = require("mongoose");
+const OrderSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  category: String,
+  city: String,
+  budget: String,
+  clientId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model("Order", OrderSchema);
